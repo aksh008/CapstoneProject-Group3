e77423e74939ca20ad1302899f4288434bc00842
thiluk <thiluk@gmail.com>
2024-12-22 16:47:44 +0530

    Update validation.py

--- harit_model/processing/validation.py
+++ harit_model/processing/validation.py
@@ -8,18 +8,47 @@ import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from PIL import Image,ImageEnhance
+import tensorflow as tf
+
+# Function to check if the file is an image
+def is_image(image):
+    try:
+        img = Image.open(image)
+        img.verify()  # Verify that it is, in fact, an image
+        return True
+    except (IOError, SyntaxError) as e:
+        return False
+
+
+# Image quality validation function
+def validate_enhance_image_quality(image):
+    
+        # Get image resolution
+    resolution = image.shape[:2]  # Height, Width
+    if resolution[0]<224 or resolution[1]<224:
+        # Resize the image
+        image = image.resize((224,224))
+
+    # Calculate brightness as mean pixel value
+    brightness = np.mean(image)
+    # Enhance brightness
+    if brightness<50:
+        enhancer = ImageEnhance.Brightness(image)
+        image = enhancer.enhance(1.2)
+    
+
+    # Calculate sharpness using Laplacian variance
+    gray_image = tf.image.rgb_to_grayscale(image).numpy()
+    sharpness = np.var(tf.image.sobel_edges(gray_image).numpy())
+    if sharpness<2:
+        # Enhance sharpness
+        enhancer = ImageEnhance.Sharpness(image)
+        image = enhancer.enhance(2.0)
+    return image
+    
 
 def evaluate_model(model, test_data):
-    """
-    Evaluate the MobileNetV2 model on validation data.
-
-    Args:
-        model: Trained Keras model.
-        val_generator: Validation data generator.
-
-    Returns:
-        None
-    """
     print("Evaluating the model...")
     val_loss, val_accuracy = model.evaluate(test_data)
     return val_loss, val_accuracy
