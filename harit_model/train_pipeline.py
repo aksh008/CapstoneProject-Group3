a2e48d1d9ceb62fbdb30c5e919fa1234cac17f99
aksh008 <aksh008@yahoo.com>
2024-12-25 17:14:55 +0530

    updted branch akshay

--- harit_model/train_pipeline.py
+++ harit_model/train_pipeline.py
@@ -10,6 +10,22 @@ from harit_model.processing.validation import evaluate_model
 from harit_model.pipeline import train_mobilenetv2
 from harit_model.processing.features import train_test_valid
 
+from clearml import Task, OutputModel
+from pipeline import parameters
+from datetime import datetime
+import tensorflow as tf
+
+task = Task.init(project_name='Harit_project_23Dec',task_name='training_task_1')
+task.set_progress(0)
+# task doing stuff
+task.set_progress(50)
+print(task.get_progress())
+# task doing more stuff
+task.set_progress(100)
+
+output_model = OutputModel(task=task, framework="tensorflow")
+task.connect(parameters)
+
 def run_training() -> None:
     """Train the model."""
     
@@ -26,10 +42,16 @@ def run_training() -> None:
     # Create and train model
     model = train_mobilenetv2(num_classes)
     print("Training the MobileNetV2 model...")
+
+    logdir = "logs/digits/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    callbacks_list = [tf.keras.callbacks.TensorBoard(log_dir=logdir),
+                      tf.keras.callbacks.ModelCheckpoint("MyHarit_model_checkpoint.keras",save_best_only=True)]    
+    # Train the model
     model.fit(
         train_data,
         validation_data=valid_data,
-        epochs=config.model_config.epochs,
+        epochs=parameters['epoch'],
+        callbacks=callbacks_list,
         batch_size=config.model_config.batch_size
     )
 
