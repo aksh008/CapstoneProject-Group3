0bf6f3e50146a745e9900322d1019d290c9c01a0
aksh008 <aksh008@yahoo.com>
2024-12-26 11:54:41 +0530

    updated

--- harit_model/pipeline_retrain.py
+++ harit_model/pipeline_retrain.py
@@ -8,6 +8,7 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 # from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers import get
 from clearml import Task, OutputModel, InputModel
+from tensorflow.keras.models import load_model
 # from train_pipeline import task
 
 from harit_model.config.core import config
@@ -36,23 +37,29 @@ def train_mobilenetv2(num_classes):
     Returns:
         model: Compiled Keras model.
     """
-    # base_model = MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
-    # base_model.trainable = False  # Freeze base layers  
-
+    base_model = MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+    base_model.trainable = False  # Freeze base layers  
+    model = Sequential([
+        base_model,
+        GlobalAveragePooling2D(),
+        Dense(256, activation='relu'),
+        Dense(38, activation='softmax')  # Adjust the number of classes as needed
+        ])
     # input_model = InputModel(model_id="b88b00c23dc54928a2c51b02de26fd38")
     # task.connect(input_model)
 
     input_model = InputModel(model_id="b88b00c23dc54928a2c51b02de26fd38")
     local_path = input_model.get_local_copy()
     print(local_path)
-    ks_model = None
+    # ks_model = None
     try:
         # Try to load the model as a full Keras model
-        ks_model = tf.keras.models.load_model(local_path)
+        model.load_weights(local_path)
+        print("Weights loaded successfully!")
     except Exception as e:
         print(f"Could not load full model: {e}")
         print("Loading weights and rebuilding architecture.")
-    task.connect(ks_model)
+    task.connect(model)
     # model = Sequential([
     #     ks_model,
     #     GlobalAveragePooling2D(),
@@ -62,10 +69,10 @@ def train_mobilenetv2(num_classes):
     optimizer = get(parameters['optimizer'])
     optimizer.learning_rate = parameters['learning_rate']  # Set learning rate directly
     
-    ks_model.compile(
+    model.compile(
         optimizer=optimizer,
         loss="categorical_crossentropy",
         metrics=["accuracy"]
     )
 
-    return ks_model
+    return model
