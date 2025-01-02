ab4ef3ac1b47f1e8ed4e0799d303b40c6c832fd8
Ranu Gontia <rgontia@amazon.com>
2025-01-02 22:30:17 +0530

    updating harit_model_api core.py file

--- harit_model_api/app/core.py
+++ harit_model_api/app/core.py
@@ -1,9 +1,21 @@
 
-import yaml
 # from strictyaml import YAML
+from pathlib import Path
+import sys
+import os
+file = Path(__file__).resolve()
+root = file.parents[1]
+sys.path.append(str(root))
 
-def load_languages(config_path="harit_model_api\\app\config.yml"):
+import yaml
+
+def load_languages():
+
     """Load the languages list from the configuration file."""
+    config_path = os.path.join(root, "app", "config.yml")
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
     return config.get("languages", [])
+
+if __name__ == "__main__":
+    load_languages()
